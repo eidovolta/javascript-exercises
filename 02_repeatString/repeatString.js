d4b292dcce5626f1c307fe81fd6034069e458e3b
eidovolta <19731799+eidovolta@users.noreply.github.com>
2022-11-01 11:46:39 +0100

    Solve 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(inputString, nTimes) {
+    if (nTimes < 0) {
+        return "ERROR";
+    }
+    resultString = "";
 
+    for (let i = 1; i <= nTimes; i++) {
+        resultString += inputString;
+    }
+
+    return resultString;
 };
 
 // Do not edit below this line
