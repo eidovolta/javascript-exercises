9dc4eba00ee301c7ea9024f504465612b0083140
eidovolta <19731799+eidovolta@users.noreply.github.com>
2022-11-01 13:38:33 +0100

    Solve 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(degreesInF) {
+  let degreesInC = (degreesInF - 32) * (5 / 9);
+  return parseFloat(degreesInC.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(degreesInC) {
+  let degreesInF = degreesInC * (9 / 5) + 32;
+  return parseFloat(degreesInF.toFixed(1));
 };
 
 // Do not edit below this line
