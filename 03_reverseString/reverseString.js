e2b532aca0b5043e71c131c398550ce707657e77
eidovolta <19731799+eidovolta@users.noreply.github.com>
2022-11-01 11:54:16 +0100

    Solve 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+/**
+ * 
+ * @param {string} stringToReverse 
+ */
+const reverseString = function(stringToReverse) {
+    // puts the string into an array to reverse
+    const arrayString = stringToReverse.split("");
 
+    arrayString.reverse();
+
+    // joins the reversed array into a return string
+    return arrayString.join("");
 };
 
 // Do not edit below this line
