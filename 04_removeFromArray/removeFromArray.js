9198dee6dc211e1a967d8891ca4acfdbb61c0b16
eidovolta <19731799+eidovolta@users.noreply.github.com>
2022-11-01 12:21:33 +0100

    Solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+/**
+ * 
+ * @param {array} inputArray 
+ * @param  {...any} wordsToRemove list of arguments to remove from the specified array
+ */
+const removeFromArray = function(inputArray, ...wordsToRemove) {
+    let indexToRemove;
 
+    for (const word of wordsToRemove) {
+        indexToRemove = inputArray.indexOf(word);
+
+        // check if the word matches to an array element
+        if (indexToRemove > -1) inputArray.splice(indexToRemove, 1);
+    }
+
+    return inputArray;
 };
 
 // Do not edit below this line
