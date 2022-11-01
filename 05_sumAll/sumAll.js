7ccc923c3e82e256ac34419878b3ba77916f5ff4
eidovolta <19731799+eidovolta@users.noreply.github.com>
2022-11-01 13:27:31 +0100

    Solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+/**
+ * Sums all the numbers (including) between the two arguments
+ * 
+ * @param {number} number1 
+ * @param {number} number2 
+ */
+const sumAll = function(number1, number2) {
+    // check if arguments are strictly numbers
+    if (!(typeof(number1) === "number" && typeof(number2) === "number")) {
+        return "ERROR";
+    }
+    else if (number1 < 0 || number2 < 0) {
+        return "ERROR";
+    }
 
+    // make sure the sum starts with the smaller number and ends with the bigger number
+    const start = number1 <= number2 ? number1 : number2;
+    const end = number1 <= number2 ? number2 : number1;
+
+    let result = 0;
+
+    for (let i = start; i <= end; i++) {
+        result += i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
