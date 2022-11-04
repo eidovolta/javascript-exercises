da726e24904eca3c7274107f419c3ad3f3a7f62a
eidovolta <19731799+eidovolta@users.noreply.github.com>
2022-11-04 15:35:11 +0100

    Solve 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+/**
+ * 
+ * @param {string} text 
+ */
+const palindromes = function (text) {
+    const processedText = text.toLowerCase().replace(/[^a-z]/g, "");
+    
+    const textArray = processedText.split("");
+    const textArrayReverse = textArray.map((x) => x);
+    textArrayReverse.reverse();
 
+    for (let i = 0; i < textArray.length; i++) {
+        if (textArray[i] !== textArrayReverse[i]) {
+            return false;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
