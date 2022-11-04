2ba7bcc415c711f35fa6ab3758892759a0ac888d
eidovolta <19731799+eidovolta@users.noreply.github.com>
2022-11-04 15:44:00 +0100

    Solve 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(memberPosition) {
+    const memberPositionParsed = parseInt(memberPosition);
+    let previousFibonacci = 0;
+    let currentFibonacci = 1;
 
+    if (memberPositionParsed < 1) {
+        return "OOPS";
+    }
+    for (let i = 1; i < memberPositionParsed; i++) {
+        let temp = currentFibonacci;
+        currentFibonacci = previousFibonacci + currentFibonacci;
+        previousFibonacci = temp;
+    }
+
+    return currentFibonacci;
 };
 
 // Do not edit below this line
