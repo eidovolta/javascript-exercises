d7edafcc4155ef903afa2039306b0fb5dade5edf
eidovolta <19731799+eidovolta@users.noreply.github.com>
2022-11-04 16:06:50 +0100

    Solve 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+    for (let person of peopleArray) {
+        if (!person.yearOfDeath) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+    }
+    const oldestPerson = peopleArray.reduce((oldest, current) => {
+        if ((oldest.yearOfDeath - oldest.yearOfBirth) > (current.yearOfDeath - current.yearOfBirth)) {
+            return oldest;
+        }
+        else {
+            return current;
+        }
+    }, {name: "default", yearOfBirth: 1950, yearOfDeath: 1950});
 
+    console.log(oldestPerson);
+    return oldestPerson;
 };
 
 // Do not edit below this line
